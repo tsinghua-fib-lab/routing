b50cd3085afa93e97e8436fb7c9bc44445ce0b2f
Jun Zhang <zhangjun990222@qq.com>
2021-08-24 19:27:01 +0800

    脚本支持自动识别目录

--- scripts/init_conan.sh
+++ scripts/init_conan.sh
@@ -2,7 +2,10 @@
 
 set -e
 
-mkdir -p ../.conan/
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+PROJECT_DIR="$(dirname ${SCRIPT_DIR})"
+
+mkdir -p ${PROJECT_DIR}/.conan/
 conan profile new default --detect
 conan profile update settings.compiler.libcxx=libstdc++11 default
 conan profile update settings.compiler.cppstd=gnu17 default
